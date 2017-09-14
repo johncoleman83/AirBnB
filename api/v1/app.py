a95973d5c906b846fb905cf00f1a095f03f41000
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-14 16:34:45 -0700

    added encrypted password to the system with new attribute name using getter and setter, updated console, and other functions that related to password updates

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,10 +2,10 @@
 """
 Flask App that integrates with AirBnB static HTML Template
 """
-from api.v1.views import app_views
+from api.v1.views import app_views, auth_blueprint
+from flasgger import Swagger
 from flask import Flask, jsonify, make_response, render_template, url_for
 from flask_cors import CORS, cross_origin
-from flasgger import Swagger
 from models import storage
 import os
 from werkzeug.exceptions import HTTPException
@@ -13,6 +13,20 @@ from werkzeug.exceptions import HTTPException
 # Global Flask Application Variable: app
 app = Flask(__name__)
 swagger = Swagger(app)
+
+# global strict slashes
+app.url_map.strict_slashes = False
+
+# flask server environmental setup
+host = os.getenv('BTCPBNB_API_HOST', '0.0.0.0')
+port = os.getenv('BTCPBNB_API_PORT', 5001)
+
+# Cross-Origin Resource Sharing
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+# app_views BluePrint defined in api.v1.views
+app.register_blueprint(app_views)
+app.register_blueprint(auth_blueprint)
 
 
 # begin flask page rendering
@@ -23,6 +37,28 @@ def teardown_db(exception):
     the current SQLAlchemy Session
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 errors, in the event that global error handler fails
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
+
+
+@app.errorhandler(400)
+def handle_404(exception):
+    """
+    handles 400 errros, in the event that global error handler fails
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
 
 
 @app.errorhandler(Exception)
@@ -49,32 +85,11 @@ def setup_global_errors():
         app.register_error_handler(cls, global_error_handler)
 
 
-def run_main_app():
-    """
-        Executes main functionality of the Flask Web App
-    """
-    # global strict slashes
-    app.url_map.strict_slashes = False
-
-    # Cross-Origin Resource Sharing
-    cors = CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
-
-    # app_views BluePrint defined in api.v1.views
-    app.register_blueprint(app_views)
-
-    # initializes global error handling
-    setup_global_errors()
-
-    # flask server environmental setup
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', 5000)
-
-    # start Flask app
-    app.run(host=host, port=port)
-
-
 if __name__ == "__main__":
     """
     MAIN Flask App
     """
-    run_main_app()
+    # initializes global error handling
+    setup_global_errors()
+    # start Flask app
+    app.run(host=host, port=port)
