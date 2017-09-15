d8c9f70f3a48bea8ba4dd54a11a8b4a3f7f51bae
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-14 23:13:54 -0700

    initialization of flasgger apidocs for states API

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -20,13 +20,13 @@ def states_no_id():
         return jsonify(all_states)
 
     if request.method == 'POST':
-        req_json = request.get_json()
-        if req_json is None:
+        req_data = request.get_json()
+        if req_data is None:
             abort(400, 'Not a JSON')
-        if req_json.get("name") is None:
+        if req_data.get("name") is None:
             abort(400, 'Missing name')
         State = CNC.get("State")
-        new_object = State(**req_json)
+        new_object = State(**req_data)
         new_object.save()
         return jsonify(new_object.to_json()), 201
 
@@ -38,25 +38,20 @@ def states_with_id(state_id=None):
         states route to handle http method for requested state by id
     """
     state_obj = storage.get('State', state_id)
+    if state_obj is None:
+        abort(404, 'Not found')
 
     if request.method == 'GET':
-        if state_id and state_obj is None:
-            abort(404, 'Not found')
-        elif state_obj:
-            return jsonify(state_obj.to_json())
+        return jsonify(state_obj.to_json())
 
     if request.method == 'DELETE':
-        if state_obj is None:
-            abort(404, 'Not found')
         state_obj.delete()
         del state_obj
         return jsonify({})
 
     if request.method == 'PUT':
-        req_json = request.get_json()
-        if state_obj is None:
-            abort(404, 'Not found')
-        if req_json is None:
+        req_data = request.get_json()
+        if req_data is None:
             abort(400, 'Not a JSON')
-        state_obj.bm_update(req_json)
+        state_obj.bm_update(req_data)
         return jsonify(state_obj.to_json())
