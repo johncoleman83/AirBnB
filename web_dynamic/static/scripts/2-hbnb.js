41f664f92fc18193c1bde6eca9eb4d2f8f82e16e
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-30 18:14:50 -0700

    debugged jQuery problem with variable

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,4 @@
 const checkedItems = [];
-const statusSignal = $('DIV#api_status')
 
 function remove (array, element) {
   const index = array.indexOf(element);
@@ -26,30 +25,19 @@ function amenCheck () {
 }
 
 function checkStatus () {
-  console.log('function called');
   $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-    console.log('passed get');
-    //statusSignal.toggleClass('available');
     if (data.status === 'OK') {
       console.log('status OK');
-      //statusSignal.toggleClass('available');
-      if (statusSignal.hasClass('available') !== true) {
-        console.log('This status signal: ' + statusSignal);
-        console.log('statusSignal does not have class available');
-        //statusSignal.toggleClass('available');
+      if (!$('DIV#api_status').hasClass('available')) {
+        $('DIV#api_status').toggleClass('available');
       }
-    } else if (statusSignal.hasClass('available') === true) {
-      console.log('else if statement reached');
-      //statusSignal.toggleClass('available');
+    } else if ($('DIV#api_status').hasClass('available')) {
+      $('DIV#api_status').toggleClass('available');
     }
   });
 }
 
-//statusSignal.toggleClass('available');
-
 $(document).ready(function () {
-  //checkStatus();
+  checkStatus();
   amenCheck();
 });
-
-$('DIV#api_status').toggleClass('available');
