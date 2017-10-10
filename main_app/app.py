82387e4e22070f7b95d92262593938575c727543
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-10-10 14:01:15 -0700

    fixed json loading errors and finalized user login functional version

--- main_app/app.py
+++ main_app/app.py
@@ -3,6 +3,7 @@
 Flask App that integrates with AirBnB static HTML Template
 """
 from flask import Flask, render_template, request, url_for
+import json
 from models import storage
 import requests
 from uuid import uuid4
@@ -11,7 +12,7 @@ from uuid import uuid4
 # flask setup
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-port = 5000
+port = 5001
 host = '0.0.0.0'
 
 
@@ -31,31 +32,32 @@ def main_index(the_id=None):
     handles request to main index, currently a login page
     """
     if request.method == 'GET':
-        return render_template('index.htmlj')
+        return render_template('index.html')
     if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+        email = request.form.get('email', None)
+        password = request.form.get('password', None)
         payload = {
             'email': email,
             'password': password
         }
         headers = {
-            'Content-Type': 'application/json'
+            'content-type': 'application/json'
         }
         url = 'http://0.0.0.0:5000/auth/login'
-        r = request.get(url, headers=headers, data=payload)
+        r = requests.post(url, headers=headers, data=json.dumps(payload))
         r_data = r.json()
         if r_data.get('error'):
-            return render_template('index.htmlj')
+            return render_template('index.html')
         auth_token = r_data.get('auth_token')
         if auth_token is None:
-            return render_template('index.htmlj')
+            return render_template('index.html')
         state_objs = storage.all('State').values()
         states = dict([state.name, state] for state in state_objs)
         amens = list(storage.all('Amenity').values())
         cache_id = uuid4()
         return render_template('places.html', states=states, amens=amens,
                                cache_id=cache_id, auth_token=auth_token)
+
 
 @app.route('/places/', methods=['GET'])
 def hbnb_filters(the_id=None):
