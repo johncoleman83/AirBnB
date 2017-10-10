903310b0cf2a6bc842e9bc7c874b954b071c3615
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-10-10 12:28:34 -0700

    adding beginning functionality for user login form

---
+++ main_app/app.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""
+Flask App that integrates with AirBnB static HTML Template
+"""
+from flask import Flask, render_template, request, url_for
+from models import storage
+import requests
+from uuid import uuid4
+
+
+# flask setup
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+port = 5000
+host = '0.0.0.0'
+
+
+# begin flask page rendering
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    after each request, this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main_index(the_id=None):
+    """
+    handles request to main index, currently a login page
+    """
+    if request.method == 'GET':
+        return render_template('index.htmlj')
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        payload = {
+            'email': email,
+            'password': password
+        }
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        url = 'http://0.0.0.0:5000/auth/login'
+        r = request.get(url, headers=headers, data=payload)
+        r_data = r.json()
+        if r_data.get('error'):
+            return render_template('index.htmlj')
+        auth_token = r_data.get('auth_token')
+        if auth_token is None:
+            return render_template('index.htmlj')
+        state_objs = storage.all('State').values()
+        states = dict([state.name, state] for state in state_objs)
+        amens = list(storage.all('Amenity').values())
+        cache_id = uuid4()
+        return render_template('places.html', states=states, amens=amens,
+                               cache_id=cache_id, auth_token=auth_token)
+
+@app.route('/places/', methods=['GET'])
+def hbnb_filters(the_id=None):
+    """
+    handles request to custom template with states, cities & amentities
+    """
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amens = list(storage.all('Amenity').values())
+    cache_id = uuid4()
+    auth_token = 'please_login'
+    return render_template('places.html', states=states, amens=amens,
+                           cache_id=cache_id, auth_token=auth_token)
+
+if __name__ == "__main__":
+    """
+    MAIN Flask App"""
+    app.run(host=host, port=port)
